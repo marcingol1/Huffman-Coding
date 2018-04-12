c697640837e5c879ef35173c3eab47237e88423c
Marcin Gol <golmarcin@gmail.com>
2018-04-12 23:31:45 +0200

    migrate to react-typescript xD

--- app/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { routerReducer as routing } from "react-router-redux";
-import { combineReducers, Reducer } from "redux";
-
-const rootReducer = combineReducers({
-  routing: routing as Reducer<any>,
-});
-
-export default rootReducer;
