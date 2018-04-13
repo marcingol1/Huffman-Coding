952f7a69bd4320bfcff410a07fa47ead488d62db
Marcin Gol <golmarcin@gmail.com>
2018-04-13 23:26:36 +0200

    add GraphNode class

--- src/utils/coding.ts
+++ src/utils/coding.ts
@@ -3,16 +3,30 @@ import generateRandomCode from './generateRandomSigns';
 
 class HuffmanCoding {
     data: Sign[];
+    root: GraphNode;
 
     constructor(data: Sign[] = generateRandomCode(5)) {
         this.data = data;
         console.info('creating new coding with: ', this.data);
     }
 
+    createRootNode(node: GraphNode) {
+        this.root = node;
+    }
+
     // first: symbols and their probabilities
     //
 }
 
-export default HuffmanCoding;
+class GraphNode {
+    parent?: GraphNode;
+    children?: GraphNode[];
+    sign?: string;
+    constructor(parent: GraphNode, children: GraphNode[], sign: string) {
+        this.parent = parent;
+        this.children = children;
+        this.sign = sign;
+    }
+}
 
-// export default HuffmanCoding;
+export default HuffmanCoding;
