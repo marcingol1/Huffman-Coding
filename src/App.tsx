653aa927de946d2b60e719751c62fa4a2bb5f76f
Marcin Gol <golmarcin@gmail.com>
2018-04-13 22:12:01 +0200

    update coding file

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './App.css';
+
+const logo = require('./logo.svg');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/utils/coding.ts
+++ src/utils/coding.ts
@@ -1,40 +1,40 @@
 class HuffmanCoding {
-    data: Array<Sign>;
-    constructor(data = []) {
+    data: Sign[];
 
+    constructor(data: Sign[] = []) {
+        this.data = data;
     }
-    //first: symbols and their probabilities
+
+    // first: symbols and their probabilities
     //
 }
 
 class Sign {
     name: string;
     p: number;
-    constructor(name = '', p = 0) {
+
+    constructor(name: string = '', p: number = 0) {
         this.name = name;
         this.p = p;
     }
-
 }
 
-//Example for A, B, C, D, E
+// Example for A, B, C, D, E
 
-/**
- * Generates array of given length with random Signs
- * @param length {number]} - max 9 for now
- */
-function generateRandomCode(length: number = 0): Array<Sign> {
+function generateRandomCode(length: number = 0): Sign[] {
+    const randomSigns: String[] = Math
+        .random()
+        .toString(36)
+        .slice(2, length + 2)
+        .split('');
 
-    const randomSigns = Math.random().toString(36).slice(2, length + 2);
     const randomPercentages = Array.from({length}, () => Math.floor(Math.random() * 100));
 
-    console.log(randomSigns);
-    console.log(randomPercentages);
-    return [];
+    return randomSigns.map( (sign: string, index: number) => new Sign(sign, randomPercentages[index]));
 }
 
-generateRandomCode(9);
+console.log(generateRandomCode(9));
 
 const coding = new HuffmanCoding();
 
-//export default HuffmanCoding;
+// export default HuffmanCoding;
